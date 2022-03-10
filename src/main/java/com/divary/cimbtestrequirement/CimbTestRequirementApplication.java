ca9f92c1de0d96598f506091f9f594de9baf410b
rakhadivary.noor <rakhadivary.noor@homecredit.co.id>
2022-03-10 15:27:35 +0700

    initial commit

---
+++ src/main/java/com/divary/cimbtestrequirement/CimbTestRequirementApplication.java
@@ -0,0 +1,13 @@
+package com.divary.cimbtestrequirement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CimbTestRequirementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CimbTestRequirementApplication.class, args);
+    }
+
+}
