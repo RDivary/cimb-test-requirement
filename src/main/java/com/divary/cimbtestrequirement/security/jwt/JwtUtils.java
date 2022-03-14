eda123b0239e0d15854ef3652b6dc1a4de5c1410
rakhadivary.noor <rakhadivary.noor@homecredit.co.id>
2022-03-14 10:10:17 +0700

    add expiration jwt

--- src/main/java/com/divary/cimbtestrequirement/security/jwt/JwtUtils.java
+++ src/main/java/com/divary/cimbtestrequirement/security/jwt/JwtUtils.java
@@ -26,7 +26,7 @@ public class JwtUtils {
         return Jwts.builder()
                 .setSubject(userPrincipal.getUsername())
                 .setIssuedAt(new Date())
-//                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
                 .claim(ROLE, userPrincipal.getAuthorities().stream().findFirst().get().toString())
                 .compact();
@@ -42,10 +42,6 @@ public class JwtUtils {
             return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(parseJwt(token)).getBody().get(ROLE, String.class);
 
         return null;
-    }
-
-    public Date getIssuedAtFromJwtToken(String token) {
-        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getIssuedAt();
     }
 
     public boolean validateJwtToken(String authToken) {
